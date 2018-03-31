2c73810c1d3f4794821a49e811121a51fbbfeb68
Attidd <ballotdeath@gmail.com>
2018-03-31 01:39:43 -0400

    updated with texture

--- components/login-form.js
+++ components/login-form.js
@@ -1,64 +1,71 @@
+import { Button, Icon } from "semantic-ui-react";
+import "semantic-ui-css/components/button.css";
+import "semantic-ui-css/components/icon.css";
 
-
-const Header = () => (
- <div></div>
-  );
-
+const Header = () => <div />;
 
 const Footer = () => (
-    <div className='hello'>
-    <p> This is just the beginning. Separate this by columns, login page will have SSO buttons at the bottom.</p>
+  <div className="footer">
+    <p>
+      {" "}
+      This is just the beginning. Separate this by columns, login page will have
+      SSO buttons at the bottom.
+    </p>
     <style jsx>{`
       p {
         color: white;
       }
-      .hello {
+      .footer {
         font: 15px Helvetica, Arial, sans-serif;
         background: black;
         padding: 10px;
         text-align: center;
-        position:fixed;bottom:0;left:0;right:0;height:30px;
+        position: fixed;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        height: 30px;
         transition: 100ms ease-in background;
         height: 10vh;
         box-shadow: 0px 0px 10px #000000;
-
       }
-      .hello:hover {
-        box-shadow: 0px 0px 10px #000000;
-
+      .footer:hover {
+        box-shadow: 0px 0px 10px yellow;
       }
     `}</style>
   </div>
-  );
+);
 
 export default () => (
   <div className="login">
-  <div className="base">
-    <h4 className="login-title">User Login</h4>
-    <hr />
-    <p>
-       Username <br/> <input id="uName" placeholder="Username" className="box curmudgeon" type="text" />
-    </p>
-    <p>
-      Password <br/> <input type="password" placeholder="Password" className="box passwd" />
-    </p>
-    <p><input type="checkbox"/>  Remember me</p>
-   <button>Login</button>
+    <div className="base">
+      <h4 className="login-title">User Login</h4>
+      <hr />
+      <p className="hov">
+        Username <br />{" "}
+        <input
+          id="uName"
+          placeholder="Username"
+          className="box curmudgeon"
+          type="text"
+        />
+      </p>
+      <p className="hov">
+        Password <br />{" "}
+        <input type="password" placeholder="Password" className="box passwd" />
+      </p>
+      <Button primary>
+        <Icon name="sign in" />
+        Login
+      </Button>
 
-    <style jsx>{`
+      <style jsx>{`
 
       .login-title {
         margin-bottom: 10px;
       }
-      button {
-        background: none;
-        border: 0;
-        box-sizing: border-box;
-        margin: 1em;
-        padding: 1em 2em;
-        box-shadow: inset 0 0 0 2px #f45e61;
-        font-weight: 700;
-      }
+
+
 
       input[type="text"] {
 
@@ -72,6 +79,12 @@ export default () => (
         margin-right: auto;
         height: 100%;
         text-align: center;
+        border: 1px rgb(76,45,48) solid;
+        border-radius: 8px;
+        background: rgba(179,179,179,0.9);
+        padding: 8px 10px;
+        box-shadow: 0px 0px 5px #000000;
+
       }
       h4 {
         text-align: center;
@@ -82,30 +95,29 @@ export default () => (
         text-align: center;
         transition: 100ms ease-in background;
       }
-      p:hover, input:focus {
+      .hov:hover, input:focus {
         background: #ccc;
         box-shadow: 0px 0px 1px #000000;
       }
 
-.box {
-  position:relative;
-  color: black;
-  display: inline-block;
-  height: 20px;
-  text-align: center;
-  transition: 0.5s;
-  border: 2px solid black;
-  -webkit-transition:0.5s;
-}
-.box:hover {
-  border: 2px solid black;
-  color: red;
-}
-
+      .box {
+        position:relative;
+        color: black;
+        display: inline-block;
+        height: 20px;
+        text-align: center;
+        transition: 0.5s;
+        border: 2px solid black;
+        -webkit-transition:0.5s;
+        border-radius: 4px;
+      }
+      .box:hover {
+        border: 2px solid black;
+        color: red;
+      }
 
     `}</style>
-  </div>
-  <Footer/>
+    </div>
+    <Footer />
   </div>
 );
-
--- components/meta.js
+++ components/meta.js
@@ -6,12 +6,14 @@ export default () => (
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta charSet="utf-8" />
       <link rel="shortcut icon" href="" />
+      <link rel='stylesheet' href='/_next/static/style.css' />
+      <link rel='stylesheet' href='/static/custom.css' />
     </Head>
     <style jsx global>{`
       body {
         font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
-        background: #b3b3b3;
-
+        background-color: #eeeeee;
+        background-image: url("/static/pattern1.jpg");
       }
       * {
         margin: 0;
