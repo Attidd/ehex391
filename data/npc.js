7cf13ffa8874913ff21dff668f5b56be57da2bcd
shu <ballotdeath@gmail.com>
2018-04-03 22:32:16 -0400

    Added placeholder NPC

---
+++ data/npc.js
@@ -0,0 +1,13 @@
+const npc = [
+{
+  "id": "1",
+  "name": "Funka",
+  "description": "A feisty Craftsman",
+  "skills": [
+  {"id":"1", "level": "1"},
+  {"id":"2", "level" "50"},
+  ];
+}
+];
+
+export default npc;
