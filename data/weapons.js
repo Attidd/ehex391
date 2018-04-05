8905408e1e97f209c7a47a20949ba90659dc2515
shu <ballotdeath@gmail.com>
2018-04-05 14:13:27 -0400

    reformatted json for weapons

--- data/weapons.js
+++ data/weapons.js
@@ -1,29 +1,28 @@
 const weapons = [
-{
-  "id": "1",
-  "name": "Shortsword",
-  "cost": "810",
-  "type": "Sword",
-},
-{
-  "id": "2",
-  "name": "Longsword",
-  "cost": "830",
-  "type": "Sword",
-},
-{
-  "id": "3",
-  "name": "Bronzesword",
-  "cost": "2,100",
-  "type": "Sword",
-},
-{
-  "id": "4",
-  "name": "Ironsword",
-  "cost": "2,800",
-  "type": "Sword"
-}
-
+  {
+    id: "1",
+    name: "Shortsword",
+    cost: "810",
+    type: "Sword"
+  },
+  {
+    id: "2",
+    name: "Longsword",
+    cost: "830",
+    type: "Sword"
+  },
+  {
+    id: "3",
+    name: "Bronzesword",
+    cost: "2,100",
+    type: "Sword"
+  },
+  {
+    id: "4",
+    name: "Ironsword",
+    cost: "2,800",
+    type: "Sword"
+  }
 ];
 
 export default weapons;
