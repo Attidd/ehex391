18b541cb0d1cc323c5f9b60b318078ff3320d9eb
shu <ballotdeath@gmail.com>
2018-04-05 21:16:02 -0400

    Can now add weapons & delete all

--- data/weapons.js
+++ data/weapons.js
@@ -1,24 +1,24 @@
 const weapons = [
   {
-    id: "1",
+    id: 1,
     name: "Shortsword",
     cost: "810",
     type: "Sword"
   },
   {
-    id: "2",
+    id: 2,
     name: "Longsword",
     cost: "830",
     type: "Sword"
   },
   {
-    id: "3",
+    id: 3,
     name: "Bronzesword",
     cost: "2,100",
     type: "Sword"
   },
   {
-    id: "4",
+    id: 4,
     name: "Ironsword",
     cost: "2,800",
     type: "Hammer"
--- pages/trading.js
+++ pages/trading.js
@@ -10,14 +10,86 @@ import {
   Table,
   Label,
   Popup,
-  Icon
+  Icon,
+  Button
 } from "semantic-ui-react";
 import Page from "../components/layout/page";
 import weapons from "../data/weapons";
+import { observable } from "mobx";
+import { observer } from "mobx-react";
 
+const store = observable({
+  itemCount: weapons.length,
+  lastItem: null,
+  weapons: weapons,
+  listItems: null
+});
+
+const handleBuyWeapon = name => () => {
+  store.weapons.push(name);
+  store.itemCount += 1;
+  store.listItems = store.weapons.map(weapon => <li key={weapon.id}>{weapon.name}</li>);
+  store.lastItem = '[ID]: ' + store.weapons.slice(-1)[0].id + ' Name: ' + store.weapons.slice(-1)[0].name + '. ' ;
+};
+
+const handleClearWeapons = () => {
+  store.weapons = [{id: 0}];
+  store.itemCount = 0;
+  store.lastItem = null;
+};
+
+const TableWeapons = observer(() => (
+                <Table.Body>
+                {store.weapons.filter(x => x.type === "Sword").map(weapon => (
+                  <Table.Row key={weapons.id}>
+                    <Table.Cell onClick={thisRowClick.bind(this, weapon.name)}>
+                      <Label color="orange" ribbon>
+                        {weapon.name}
+                      </Label>
+                    </Table.Cell>
+                    <Table.Cell>{weapon.type}</Table.Cell>
+                    <Table.Cell>{weapon.cost}</Table.Cell>
+                    <Table.Cell>{weapon.id}</Table.Cell>
+                  </Table.Row>
+                ))}
+              </Table.Body>)
+  );
+
+const NewWeapon = observer(() => (
+  <Segment textAlign="center" color="black" className="BuyWeapons">
+        <Button primary onClick={handleBuyWeapon({
+    id: 1 + store.weapons.slice(-1)[0].id,
+    name: "Wooden Sword",
+    cost: "810",
+    type: "Sword"
+  })}>Wooden Sword(Observer)</Button>
+                <Button primary onClick={handleBuyWeapon({
+    id: 1 + store.weapons.slice(-1)[0].id,
+    name: "Worthless Hammer",
+    cost: "0",
+    type: "Hammer"
+  })}>Worthless Hammer(Observer)</Button>
+                <Button color="red" onClick={handleClearWeapons}>Delete Weapons</Button>
+                </Segment>
+
+));
+
+const WeaponCount = observer(() => (
+  <Header textAlign="center" as="h2">
+  {store.lastItem} <br/>
+    Weapons ({store.itemCount})
+  </Header>
+));
+
+const SwordCount = observer(() => (
+                <Menu.Item as="a">
+                Swords ({weapons.filter(x => x.type === "Sword").length})
+              </Menu.Item>
+              ));
 const styleGrid1 = { width: 130, height: 130 };
 const sizeGrid1 = "big";
 const thisRowClick = weapon => alert(weapon);
+const TodoView = observer(({ todo }) => <div>{todo.title}</div>);
 const Trading = () => (
   <div>
     <Page>
@@ -41,6 +113,8 @@ const Trading = () => (
         <Header textAlign="center" as="h2">
           Data query
         </Header>
+        <Divider section />
+        <NewWeapon />
         <Divider section />
 
         <Grid columns={2}>
@@ -82,15 +156,11 @@ const Trading = () => (
             </Menu>{" "}
           </Grid.Column>
           <Grid.Column>
-            <Header textAlign="center" as="h2">
-              Weapons ({weapons.length})
-            </Header>
+            <WeaponCount />
             <Divider section />
 
             <Menu attached="top" compact inverted widths={3}>
-              <Menu.Item as="a">
-                Swords ({weapons.filter(x => x.type === "Sword").length})
-              </Menu.Item>
+            <SwordCount/>
               <Menu.Item as="a">Shields </Menu.Item>
               <Menu.Item as="a">Armor ({weapons.length})</Menu.Item>
             </Menu>
@@ -99,20 +169,9 @@ const Trading = () => (
                 <Table.HeaderCell>Name</Table.HeaderCell>
                 <Table.HeaderCell>Type</Table.HeaderCell>
                 <Table.HeaderCell>Cost (Coins)</Table.HeaderCell>
+                <Table.HeaderCell>ID</Table.HeaderCell>
               </Table.Header>
-              <Table.Body>
-                {weapons.filter(x => x.type === "Sword").map(weapon => (
-                  <Table.Row key={weapons.id}>
-                    <Table.Cell onClick={thisRowClick.bind(this, weapon.name)}>
-                      <Label color="orange" ribbon>
-                        {weapon.name}
-                      </Label>
-                    </Table.Cell>
-                    <Table.Cell>{weapon.type}</Table.Cell>
-                    <Table.Cell>{weapon.cost}</Table.Cell>
-                  </Table.Row>
-                ))}
-              </Table.Body>
+              <TableWeapons/>
             </Table>
             <Segment textAlign="center" attached="bottom" inverted>
               Color should represent quality, row color represents rarity.
